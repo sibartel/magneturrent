96899f75dba1dd954f296bf399d6e0a72f7064f8
Arvius <arvius@web.de>
2019-07-14 13:58:42 +0200

    Added interface implementation.

---
+++ src/interface.c
@@ -0,0 +1,46 @@
+/**
+ * @file interface.c
+ * @author Silas Bartel (arvius@web.de)
+ * @brief Library responsible for the binary data communication.
+ * @version 0.1
+ * @date 2019-07-14
+ *
+ */
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <assert.h>
+
+#include "interface.h"
+#include "uart.h"
+
+/**
+ * @brief Magic byte used by the interface.
+ */
+#define INTERFACE_MAGIC_BYTE 0xbeef;
+
+/**
+ * @brief Structure used to pack the data
+ */
+typedef union {
+    struct {
+        uint8_t magic_byte;
+        uint8_t status;
+        float payload;
+    }:
+    uint8_t raw[6];
+} InterfaceDataFrame_t;
+
+/**
+ * @brief Packs and sends a status and a payload.
+ *
+ * @param status current status of the model
+ * @param payload data to be send
+ */
+void interface_send(uint8_t status, float payload) {
+    InterfaceDataFrame_t data_frame;
+    data_frame.magic_byte = INTERFACE_MAGIC_BYTE;
+    data_frame.status = status;
+    data_frame.payload = payload;
+    uart_send((uint8_t*) &(data_frame.raw), sizeof(data_frame.raw));
+}
---
+++ src/interface.h
@@ -0,0 +1,15 @@
+/**
+ * @file interface.h
+ * @author Silas Bartel (arvius@web.de)
+ * @brief Library responsible for the binary data communication.
+ * @version 0.1
+ * @date 2019-07-14
+ * 
+ * If you search for the documentation,
+ * have a look at the source file.
+ *
+ */
+
+#pragma once
+
+void interface_send(uint8_t status, float current);
