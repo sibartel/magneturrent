f05eb5d66350c4eb0300538575bf32adc0e92dcf
Arvius <arvius@web.de>
2019-07-17 14:06:22 +0200

    Fixed heartbeat implementation.

--- src/heartbeat.c
+++ src/heartbeat.c
@@ -16,6 +16,9 @@
 #include "driverlib/sysctl.h"
 #include "driverlib/gpio.h"
 
+volatile uint32_t heartbeat_sysmillis = 0;
+void hearbeat_systick_handler();
+
 /**
  * @brief Initializes the heartbeat led of the dk-tm4c123g eval board.
  * 
@@ -27,6 +30,8 @@ void heartbeat_init() {
 
     // Enable SysTick Timer
     SysTickPeriodSet(1000);
+    SysTickIntRegister(hearbeat_systick_handler);
+    SysTickIntEnable();
     SysTickEnable();
 }
 
@@ -38,12 +43,20 @@ void heartbeat_init() {
  */
 void heartbeat_process() {
     static uint32_t last_beat = 0;
-    if(SysTickValueGet() - last_beat > 1000) {
-        last_beat = SysTickValueGet();
+    if(heartbeat_sysmillis - last_beat > 4000) {
+        last_beat = heartbeat_sysmillis;
         // Time for some beats. Funk it up.
-        if(GPIOPinRead(GPIO_PORTG_BASE, GPIO_PIN_2))
+        if(!GPIOPinRead(GPIO_PORTG_BASE, GPIO_PIN_2))
             GPIOPinWrite(GPIO_PORTG_BASE, GPIO_PIN_2, GPIO_PIN_2);
         else
             GPIOPinWrite(GPIO_PORTG_BASE, GPIO_PIN_2, 0);
     }
+}
+
+/**
+ * @brief Intenral handler gets called if systick overflows.
+ * 
+ */
+void hearbeat_systick_handler() {
+    heartbeat_sysmillis++;
 }
