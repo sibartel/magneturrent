893cddbb75ce5f935e92e904d7235d5cb339bff0
Arvius <arvius@web.de>
2019-07-17 14:36:15 +0200

    Updated magdecode to new format.

--- magdecode.py
+++ magdecode.py
@@ -1,14 +1,21 @@
 import serial
+import struct
 
-
-ser = serial.Serial('/dev/ttyACM2', 115200)
+ser = serial.Serial('/dev/ttyACM1', 115200)
 xs = []
 ys = []
 
 ser.reset_input_buffer()
 
 while True:
-    data_raw = ser.read(4)
-    print(int.from_bytes(data_raw, byteorder="little", signed=True) / 100000000)
+    while(ser.inWaiting() >= 6):
+        magic_byte = struct.unpack('B', ser.read(1))[0]
+        if magic_byte != 0xbe:
+            continue
+        
+        status = struct.unpack('B', ser.read(1))[0]
+
+        current = struct.unpack('<f', ser.read(4))[0]
+        print(current)
 
 ser.close()
